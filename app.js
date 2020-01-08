fde0d143966b7defc0ac24ba0393c18eb33e7610
unknown <nanc90997@gmail.com>
2020-01-09 02:09:44 +0700

    update

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = 3000;
 app.use('/static', express.static(path.join(__dirname, 'public')))
 console.log(__dirname);
 app.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, './views/index.html'))
+    res.sendFile(path.join(__dirname, './index.html'))
 })
 app.get('/Contact', (req, res, next) => {
     res.sendFile(path.join(__dirname, './views/Contact.html'))
