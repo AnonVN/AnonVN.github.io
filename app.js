ece3407e58a21cb6b26b39668ec807d619dc3a98
unknown <nanc90997@gmail.com>
2020-01-09 17:00:10 +0700

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require('path');
 const app = express()
-const port = 3000;
+const port = process.env.PORT || 3000;
 // const post = require('../Node2/html/post.js');
 // const mongoose = require('mongoose')
 // mongoose.Promise = global.Promise
