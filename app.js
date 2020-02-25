bea4c2e86ec95c4995cec6736e5f78366235a154
unknown <nanc90997@gmail.com>
2020-02-26 00:18:37 +0700

    haha

--- app.js
+++ app.js
@@ -1,27 +1,13 @@
 const express = require("express");
 const path = require('path');
 const app = express()
-const port = process.env.PORT || 3000;
-// const post = require('../Node2/html/post.js');
-// const mongoose = require('mongoose')
-// mongoose.Promise = global.Promise
+const port = 3000;
 
-
-// linh.use('/login',post)
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
 console.log(__dirname);
 app.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname, './views/index.html'))
-})
-app.get('/Contact', (req, res, next) => {
-    res.sendFile(path.join(__dirname, './views/Contact.html'))
-})
-app.get('/services', (req, res, next) => {
-    res.sendFile(path.join(__dirname, './views/services.html'))
-})
-app.get('/Portfolio', (req, res, next) => {
-    res.sendFile(path.join(__dirname, './views/Portfolio.html'))
 })
 
 
