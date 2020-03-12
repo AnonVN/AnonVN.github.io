65bd93e3681cd8743671f4d1939aedebf2b98f6e
unknown <nanc90997@gmail.com>
2020-03-12 17:01:44 +0700

    update

--- app.js
+++ app.js
@@ -3,14 +3,48 @@ const path = require('path');
 const app = express()
 const port = 3000;
 const apiRouter = require('./routes/api');
-// const post = require('../Node2/html/post.js');
-// const mongoose = require('mongoose')
-// mongoose.Promise = global.Promise
-var bodyPaser = require("body-parser")
+
+const bodyPaser = require("body-parser")
 app.use(bodyPaser.urlencoded({extended:false}))
 app.use(bodyPaser.json())
 
-// linh.use('/login',post)
+const passport = require('passport'),
+FacebookStrategy = require('passport-facebook').Strategy;
+
+app.use(passport.initialize());
+
+
+passport.use(new FacebookStrategy({
+    clientID: '2679285842308625',
+    clientSecret:'9949:5802f5becbcf006eca0c14865c3',
+    callbackURL: "https://3d7bd8ff.ngrok.io/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+   console.log('get ok');
+      done(null, profile);
+  }
+));
+
+
+passport.serializeUser(function  (user , done) {
+    console.log('serializeUser', user);
+    done(null,user)
+})
+
+passport.deserializeUser(function  (id, done) {
+    console.log('deserializeUser', user);
+    done(null,user)
+});
+
+
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/index',
+                                      failureRedirect: '/' }));
+
+
 app.use('/api', apiRouter);
 app.use('/static', express.static(path.join(__dirname, 'public')))
 console.log(__dirname);
---
+++ config/auth.js
@@ -0,0 +1,9 @@
+// import { models } from "mongoose";
+
+// modele.exports = {
+//     'facebookAuth' : {
+//         'clientID' : '203398384337582',
+//         'clientSecret' : '57262ef42f58e60fab53048fb42e67c3',
+//         'callbackURL' : 'https://84c646f4.ngrok.io/auth/facebook/callback'
+//     }
+// }
